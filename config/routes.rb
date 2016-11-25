ee2cf193b8a40bfff935f2715f4a84641b720ddd
heaenwie <marta.eleonora.ciesielska@gmail.com>
2016-11-25 09:30:15 +0100

    Add views and controller
    Added views and controllers for 2 models:
    Diary & Entry that belongs to the Diary

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,32 @@
 Rails.application.routes.draw do
+  get 'diaries/new'
+
+  get 'diaries/create'
+
+  get 'diaries/update'
+
+  get 'diaries/edit'
+
+  get 'diaries/destroy'
+
+  get 'diaries/index'
+
+  get 'diaries/show'
+
+  get 'entries/new'
+
+  get 'entries/create'
+
+  get 'entries/update'
+
+  get 'entries/edit'
+
+  get 'entries/destroy'
+
+  get 'entries/index'
+
+  get 'entries/show'
+
   devise_for :users
   root 'main#index'
   get 'main/index'
